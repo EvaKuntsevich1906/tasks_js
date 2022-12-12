c1b5144bbb7cb57bf62280732aecc30802e5df5b
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-12-12 22:12:02 +0300

    task for numbers

---
+++ Number/4/app.js
@@ -0,0 +1,4 @@
+//Найдите квадратный корень из 245 (Math.sqrt())
+
+const  a = 245;
+console.log(Math.sqrt(a))
---
+++ Number/5/app.js
@@ -0,0 +1,7 @@
+// Даны числа 4, -2, 5, 19, -130, 0, 10. 
+// Найдите минимальное и максимальное число(Math.min(), Math.max())
+
+const arr = [4, -2, 5, 19, -130, 0, 10]
+
+console.log(Math.min(...arr))
+console.log(Math.max(...arr))
