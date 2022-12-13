d23998170a3882685e647d3bef2351b5d832b06d
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-12-13 12:39:27 +0300

    new tasks from string

---
+++ String/4/app.js
@@ -0,0 +1,8 @@
+// Задача. У вас есть массив [‘I’, ‘like, ‘to’, ‘learn’, ‘something’, ‘new’]. Преобразовать в строку ‘I like to learn something new’
+
+
+const str = ['I', 'like', 'to', 'learn', 'something', 'new'];
+
+alert(str.join(',').replaceAll(","," "))
+
+
---
+++ String/6/app.js
@@ -0,0 +1,7 @@
+// Задача. "HTML JavaScript PHP" преобразовать в "HTML-JavaScript-PHP" 
+
+const str = "HTML JavaScript PHP"
+
+console.log(str.replaceAll(" ","-"));
+
+
