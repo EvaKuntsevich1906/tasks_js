bf35553f644410b0caaa7891985e45872055a3f2
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-12-13 22:31:41 +0300

    new tasks for numbers

--- Number/24/app.js
+++ Number/24/app.js
@@ -2,3 +2,18 @@
 // Пользователь вводит число n. Данное число – количество элементов массива. 
 // Создайте массив рандомных чисел, состоящий из n-кол-ва. Найдите наименьшее 
 // и наибольшее число.
+
+let value = +prompt();
+let arr = [];
+
+for (let i = 0; i < value; i++) {
+    let num = Math.floor(Math.random() * 100);
+    arr.push(num)
+}
+
+let maxValue = Math.max(...arr);
+let minValue = Math.min(...arr);
+
+alert(arr);
+alert(`Максимальное число в массиве это ${maxValue}`)
+alert(`Минимальное число в массиве это ${minValue}`)
