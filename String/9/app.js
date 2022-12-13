f5bd1ecf0814f926d6d9655612a73c1bd761f2ca
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-12-14 00:00:26 +0300

    new tasks for string

---
+++ String/9/app.js
@@ -0,0 +1,13 @@
+// Дана строка 'aaa bbb ccc'. Вырежите из нее слово 
+// 'bbb' тремя разными способами (через substr, substring, slice).
+
+let str = "aaa bbb ccc";
+
+let strSubstr = str.substr(4,3);
+alert(strSubstr);
+
+let strSubstring = str.substring(4,7);
+alert(strSubstring);
+
+let strSlice = str.slice(4,7);
+alert(strSlice)
