f686c14ff982b00168fdd770f54b8a4d3817a4bd
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-12-15 14:01:47 +0300

    new tasks for string

--- String/13/app.js
+++ String/13/app.js
@@ -1,6 +1,25 @@
-// На вход программе подается переменная date, в котрой лежит дата в формате 
-// 'xxxx-xx-xx'. Преобразуйте эту дату в формат 'xx/xx/xxxx’.
+//Дана строка 'я учу javascript!'. 
+// 1. Найдите количество символов в этой строке. 
+// 2. Вырежите из нее слово 'учу' и слово 'javascript' тремя разными способами 
+// (через substr, substring, slice). 
+// 3. Найдите позицию подстроки 'учу'.
 
-let date = "2025-12-31";
+// пункт 1
 
-alert(date.replaceAll("-","/"))
+let str = "я учу javascript!"
+let length = str.length;
+alert (`Длина строки str = ${length}`);
+
+
+// пункт 2
+
+
+let strSubstr = str.substr(2);
+alert(strSubstr);
+
+let strSubstring = str.substring(2);
+alert(strSubstring);
+
+let strSlice = str.slice(2);
+alert(strSlice);
+
