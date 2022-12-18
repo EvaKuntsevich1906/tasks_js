fda755b94bb620c9789a66f975c34f7489c6da35
evakuntsevich19 <evakuntsevich1906@gmail.com>
2022-12-18 20:19:02 +0300

    new task from numbers

--- Number/35/app.js
+++ Number/35/app.js
@@ -2,3 +2,14 @@
 // чисел. Предварительно изучите модуль Math и в отдельности Math.min и 
 // Math.max
 
+let FirstNumber = +prompt(`Введите число`);
+let SecondtNumber = +prompt(`Введите число`);
+let ThirdNumber = +prompt(`Введите число`);
+let FourthNumber = +prompt(`Введите число`);
+let FifthNumber = +prompt(`Введите число`);
+
+let a = Math.max(FirstNumber, SecondtNumber, ThirdNumber, FourthNumber, FifthNumber);
+let b = Math.min(FirstNumber, SecondtNumber, ThirdNumber, FourthNumber, FifthNumber);
+
+alert(`Наибольшее число из всех введенных чисел =  ${a}`);
+alert(`Наименьшее число из всех введенных чисел =  ${b}`);
