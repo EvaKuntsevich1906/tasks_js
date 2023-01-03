02511bb91e6cbe96020a0b1fb2177e00a6bca6a3
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-03 14:28:11 +0300

    new tasks

--- String/31/app.js
+++ String/31/app.js
@@ -1,5 +1,13 @@
 //Развернуть строку не используя reverse
 
 
-let str = prompt();
+let firststr = prompt();
+// Переворот строки с помощью цикла for 
+
+let twostr = "";
+
+for (let i = firststr.length - 1; i>=0; i--) {
+    twostr += firststr[i]
+}
+alert(twostr)
 
