667b1bff252f8bad537caaacc635f6e62af07feb
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-16 00:44:54 +0300

    new tasks

--- Context/10/app.js
+++ Context/10/app.js
@@ -6,11 +6,11 @@ let s = prompt("Введите букву");
 let str = prompt("Введите строку");
 
 const counterChecking = (s, str) => {
-let counter = 0; 
-str.split("").forEach(el => {
-    if (el.includes(s)) counter ++
-})
-return counter 
+    let counter = 0;
+    str.split("").forEach(el => {
+        if (el.includes(s)) counter++
+    })
+    return counter
 }
 
-console.log(counterChecking(s,str));
+console.log(counterChecking(s, str));
