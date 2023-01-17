3aa775372e8714299009fe76a9fb10669a943614
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-17 23:48:01 +0300

    new tasks

---
+++ Object/14/app.js
@@ -0,0 +1,11 @@
+// Вычислите сумму массива целых чисел статичного массива используя рекурсию
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+function recurs (arr) {
+  if (n <= 1) return 1
+  if (arr[i].isInteger())
+  return n * recurs(n-1)
+  recurs()
+}
+recurs(arr)
