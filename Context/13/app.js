1db52fbaa15b6fe1299112a17100583b61a72807
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-19 19:53:02 +0300

    new tasks

---
+++ Context/13/app.js
@@ -0,0 +1,22 @@
+// Напишите функцию, принимающую в качестве параметра статичный объект.
+// Функция возвращает количество пар ключ / значение. IIFE
+
+let obj = {
+   value_1: "value_1",
+   2: "value_2",
+   value_3: "value_3",
+   4: "value_4",
+   value_5: "value_5",
+   6: "value_6"
+}
+
+function doCounter (obj_) {
+    let count = 0
+    for (let key in obj_) {
+        count += 1
+    }
+    return count;
+}
+
+let res_call = doCounter(obj)
+console.log(res_call);
