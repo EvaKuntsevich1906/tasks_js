fda149ba17978f193fee5a0e43f6dc95a3c26200
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-20 23:23:40 +0300

    new tasks

--- Number/56/app.js
+++ Number/56/app.js
@@ -1,8 +1,13 @@
-// Вы вводите 2 числа a и b. Вам необходимо решить пример (2(а-b)-4(b-a))/2
+// Пользователь вводит число с клавиатуры. Нужно вывести на экран сколько в этом
+// числе цифр, а также положительное оно или отрицательное. Например, "Число " +
+// num + " положительное". (length)
 
+let value = +prompt("Введите число");
 
-let a = +prompt(`Введите число а`);
-let b = +prompt(`Введите число b`);
+let valueIsSctring = value.toString().split("");
 
-let result = (2*(a-b) -4(b-a))/2; 
-console.log(`Результат вычислений - ${result}`);
+if (valueIsSctring[0] = !isNaN(valueIsSctring[0])) {
+    console.log(`Это положительное число, длина числа = ${valueIsSctring.length}`);
+} else {
+    console.log(`Это отрицательное число, длина числа = ${valueIsSctring.length - 1}`);
+}
