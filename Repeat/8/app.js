59576aacf0a2f2668ce94da44ee7327945a97294
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-23 22:58:11 +0300

    new tasks

--- Repeat/8/app.js
+++ Repeat/8/app.js
@@ -9,7 +9,7 @@ let obj = {
 let newObj = {
     id: 10, 
     name: "Hanna",
-    years: function (a,b) {
+    years: function  generationYears() {
         console.log(this.id);
     }
 }
@@ -17,8 +17,4 @@ let newObj = {
 // console.log(newObj.years());
 console.log(newObj.years.call(obj)) // в данном случае id-шник текущего обьекта ЕДИНОРАЗОВО сменятся на указанный
 console.log(newObj.years()) // поскольку предыдущая смена была ЕДИНОРАЗОВОЙ акцией, id вернулось на свое место и равно 10
-
-const newContext = newObj.years.bind(obj); 
-console.log(newContext);
-console.log(newContext);
-console.log(newContext);
+console.log(newObj.years.bind(obj)) 
