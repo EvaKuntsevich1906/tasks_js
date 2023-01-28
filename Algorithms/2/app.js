213196a6146e815b49f9c38c929d3d9efe7c9d7e
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-01-28 21:57:24 +0300

    new tasks with binare search

--- Algorithms/2/app.js
+++ Algorithms/2/app.js
@@ -3,19 +3,15 @@
 // буквы, символ, длина не менее 8 символов)
 
 let pwdString = prompt("Введите ваш пароль. Пароль должен быть НАДЕЖНЫМ.");
-let password = Array(pwdString)
+let password = pwdString.split("")
+console.log(password)
 function checkpassword(password_) {
     let lettersUpper = 0;
     let lettersLower = 0;
     let numbers = 0;
     for (let i = 0; i < password_.length; i++) {
         if (password_.length >= 8) {
-            (password_[i] === password_[i].toLowerCase()) ? lettersLower++ : alert("Для надежности вашего пароля не хватает букв в мал. регистре");
-            (password_[i] === password_[i].toUpperCase()) ? lettersUpper++ : alert("Для надежности вашего пароля не хватает букв в бол. регистре");
-            (!isNaN(password_[i])) ? numbers++ : alert("Для надежности вашего пароля не хватает букв в чисел");
-
-            let symbols = password_.length - (numbers + lettersLower + lettersUpper)
-                (symbols > 0) ? alert("Вы ввели надежный пароль") : alert("Для надежности вашего пароля не хватает символов")
+            (password_[i] === password_[i].toLowerCase()) ? lettersLower += 1 : lettersUpper += 1; 
         } else alert("Ваш пароль слишком маленький. Введите пароль длиной минимум 8 символов")
     }
 }
