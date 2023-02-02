c5cd4be9cd67cd52872a44345cf23e4b10ddb684
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-03 00:52:43 +0300

    new tasks in folder regex

--- Regex/6/app.js
+++ Regex/6/app.js
@@ -1,20 +1,23 @@
-// На вход подается строка в виде числа. Необходимо написать регулярное
-// выражение. Если строка состоит только из чисел, то вывести булевое true, в
-// противном случае бросить исключение и обработать
+// На вход подается строка вида: путь к файлу. Необходимо написать регулярное
+// выражение для данной строки. Если же строка подходит под регулярное
+// выражение, то вывести булевое true, в противном случае бросить исключение и
+// обработать
+// C:\Users\Admin\Desktop\test.txt -> true
+// C:/Users/Admin/Desktop/test.txt -> true
 
-const value = +prompt("Введите число");
+const route = prompt("Введите url");
+//https://regex101.com/
 
 
-function checking(value) {
+function checking(route) {
     try {
-        if (!/^[0-9]$/gm.test(value)) {
-            throw new Error("Это не цифры")
-        }else {
-           return true
-        }
+        if (!/^[C|D]+\:\/\\+[a-zA-Zа-яА-Я0-9]+\\[а-яА-ЯA-Za-z]+\\[a-zA-Zа-яА-Я]+\[a-zA-Zа-яА-Я]+\.[a-zA-Zа-яА-Я]+$/gm.test(route))
+            throw new Error("Это не url")
+      
     } catch (err) {
         return err.message
     }
 }
-const result = checking(value); 
+const result = checking(route); 
 console.log(result);
+
