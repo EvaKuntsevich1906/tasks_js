5424d44c84b2a976b9002cb5e9e17b108dd3e107
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-02 22:37:21 +0300

    new tasks in folder regex

---
+++ Regex/6/app.js
@@ -0,0 +1,20 @@
+// На вход подается строка в виде числа. Необходимо написать регулярное
+// выражение. Если строка состоит только из чисел, то вывести булевое true, в
+// противном случае бросить исключение и обработать
+
+const value = +prompt("Введите число");
+
+
+function checking(value) {
+    try {
+        if (!/^[0-9]$/gm.test(value)) {
+            throw new Error("Это не цифры")
+        }else {
+           return true
+        }
+    } catch (err) {
+        return err.message
+    }
+}
+const result = checking(value); 
+console.log(result);
