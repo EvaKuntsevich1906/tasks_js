37c1ab237b2e437e0cee3d49cf8a0eca66ac57dc
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-08 22:33:26 +0300

    new folder with tasks

--- Events_DOM/5/app.js
+++ Events_DOM/5/app.js
@@ -7,11 +7,15 @@ function messageOutput() {
 }
 
 const button = document.querySelector(".btn");
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
     try {
         const input = document.querySelector(".inp");
-        if (input<0) throw new Error ("Значение не может быть меньше нуля или ровняться нулюю")
- 
+        if (input.value < 0) throw new Error("Значение не может быть меньше нуля или ровняться нулю.Повторите ввод")
+        const arr = [1, 2];
+        for (let i = 2; i < input.value; i++) {
+            arr.push(arr[i - 1] + arr[i - 2])
+        }
+        alert(arr)
     } catch (err) {
         alert(err.message)
     }
