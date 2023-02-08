349870ebdd7e2329db998edb2f740a433f5ba243
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-08 23:45:31 +0300

    new folder with tasks

--- Events_DOM/10/app.js
+++ Events_DOM/10/app.js
@@ -1,3 +1,10 @@
 // После нажатия на кнопку «Разблокировать» мы можем вносить изменения в
 // инпут. После нажатия на заблокировать данное действие запрещается, тем самым
 // выключая возможность редактирования значения инпута
+
+const button1 = document.querySelector(".btn1");
+const button2 = document.querySelector(".btn2");
+
+button1.addEventListener("click", function newStyle() {
+    (button.style.backgroundColor == "blue") ? button.style.backgroundColor = "red" : button.style.backgroundColor = "blue"
+})
