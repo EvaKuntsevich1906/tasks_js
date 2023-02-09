2a0e23b7375efa0420ac890968ced64864005351
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-09 23:37:01 +0300

    new folder with tasks

--- Events_DOM/10/app.js
+++ Events_DOM/10/app.js
@@ -2,16 +2,18 @@
 // инпут. После нажатия на заблокировать данное действие запрещается, тем самым
 // выключая возможность редактирования значения инпута
 
-const button1 = document.querySelector(".btn1");
-const button2 = document.querySelector(".btn2");
 const input = document.querySelector(".inp");
 
-function updteValueInInput () {
-    if (button1.addEventListener("click", (input_) =>
-            input_.disabled = true
-        ));
-    else if ((button2.addEventListener("click", (input_) =>
-            input_.disabled = false
-        )));
-}(input)
+const button1 = document.querySelector(".btn1");
+const button2 = document.querySelector(".btn2");
+
+const updteValueInInput = (status) => {
+(status) ? input.disabled = true : input.disabled = false 
+}
+
+
+
+button1.addEventListener("click", () => updteValueInInput(true))
+button2.addEventListener("click", () => updteValueInInput(false))
+
 
