df76952d5443a921c5ba5dc475fe7fb2ede5fe80
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-19 00:40:39 +0300

    new  tasks

--- ObjMethods/4/app.js
+++ ObjMethods/4/app.js
@@ -1,2 +1,25 @@
 // На входе объект. Ключи и значения - автоинкремент (генерируется автоматически
 //     от 1 до n). Написать функцию на поиск в объекте только четных ключей. 
+const n = 10;
+let obj = {
+
+}
+
+function checkObj(n, obj) {
+    try {
+        for (let i = 0; i < n; i++) {
+            obj[i] = i
+        }
+        let arr = []
+        for (let key in obj) {
+            if (key % 2 === 0) arr.push(key)
+        }
+        if (!arr.length) throw new Error("В объекте нет четных ключей")
+        const finallyArr = arr.filter(el => el != 0)
+        alert(`Все четные ключи : ${finallyArr}`)
+    } catch (err) {
+        alert(err.message)
+    }
+
+}
+const result = checkObj(n, obj)
