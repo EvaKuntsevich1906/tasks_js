c297c225bb8bd104ceeef998ee49748429365cc8
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-19 00:13:46 +0300

    new  tasks

--- ObjMethods/2/app.js
+++ ObjMethods/2/app.js
@@ -1,2 +1,22 @@
 // Написать функцию на подсчет количества пар ключ значение в объекте. Добавить
 // проверки 
+
+const obj = {
+    name: "Eva",
+    gender: "Female",
+    years: 20,
+    hb: "19.06.2002",
+}
+
+function findCountElementsInObject(obj) {
+    try {
+        const arr = Object.entries(obj)
+        alert(`Длина объекта равна = ${arr.length}`)
+        if (arr.length == 0) throw new Error("Объект не имеет длины")
+    } catch (err) {
+        alert(err.messagw)
+    }
+}
+
+
+const result = findCountElementsInObject(obj)
