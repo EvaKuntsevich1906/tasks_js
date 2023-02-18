0b83cc94fbc6c66915355954893c6d23de483ccd
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-19 00:21:21 +0300

    new  tasks

--- ObjMethods/3/app.js
+++ ObjMethods/3/app.js
@@ -1,2 +1,25 @@
 // На входе статичный объект и строка str. Написать функцию на поиск в объекте
 // значения str. Values
+
+const obj = {
+    name: "Eva",
+    gender: "Female",
+    years: 20,
+    hb: "19.06.2002",
+
+}
+
+const str = "Eva";
+
+function findKeysWithValues(obj, str) {
+    try {
+    let arr = Object.values(obj); 
+    if(arr.includes(str)) {
+        alert("Да, такое значение есть")
+    }else throw new Error ("Такого значения в объекте не имеентся")
+    } catch (err) {
+        alert(err.message)
+    }
+}
+
+const result = findKeysWithValues(obj, str)
