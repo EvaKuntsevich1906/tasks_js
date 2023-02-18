9f08fb63c03480bd6f23798c472874ac95b04a2b
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-19 00:59:02 +0300

    new  tasks

--- ObjMethods/6/app.js
+++ ObjMethods/6/app.js
@@ -2,3 +2,22 @@
 // Ключи - автоинкремент (генерируется автоматически от 1 до n). Значение каждого
 // ключа – рандомное число от 1 до 100. Вывести каждое значение объекта с
 // помощью values
+
+const n = 50;
+let obj = {}
+
+function checkObj(n, obj) {
+    try {
+        for (let i = 0; i < n; i++) {
+            obj[i] = Math.floor(Math.random() * 100)
+        }
+        let arr = Object.values(obj)
+        if (arr.length == 0) {
+            throw new Error("В объекте нет пар, увеличьте число n")
+        } else alert(`Значения ключей в объекте ${arr}`)
+    } catch (err) {
+        alert(err.message)
+    }
+
+}
+const result = checkObj(n, obj)
