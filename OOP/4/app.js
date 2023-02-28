0846c018c59406ca8cb667e4a8846a0472bce053
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-02-28 23:23:35 +0300

    task

--- OOP/4/app.js
+++ OOP/4/app.js
@@ -1,17 +1,32 @@
-// Реализуйте класс Singer, который будет иметь следующие свойства: name,
-// surname. Также класс должен иметь метод getАutograph, который будет выводить
-// “{name} {surname), с наилучшими пожеланиями”. 
+// Реализуйте класс MathСalculation. В него передается число n – количество
+// элементов массива. На основании числа формируется динамический массив из n
+// элементов внутри класса. Создать функцию для подсчета всех четных чисел
+// массива. Добавить проверки на ввод
 
-class Singer {
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
+class MathCalculation {
+    constructor(n) {
+        this.n = n;
+    }
+    corrected  =  () =>  {
+        if (isNaN(this.n)) throw new Error("Введите числовые данные")
+        return true
     }
 
-    getAutograph() {
-        return `${this.name} ${this.surname}  с наилучшими пожеланиями`
+   createArray =  () =>  {
+        try {
+            this.corrected();
+            let arr = [];
+            let sum = 0
+            for (let i = 0; i<this.n; i++) arr.push(Math.floor(Math.random() * 100));
+            for (let i = 0; i<arr.length; i++) {
+                (arr[i] %2 === 0)  ? sum += arr[i] : null; 
+            }
+            return alert(`Массив : ${arr}, четные  числа (сумма) ${sum}`)
+        } catch (err) {
+            alert(err.message)
+        }
     }
 }
-const singer = new Singer(`John`, `Smith`);
+const calculation = new MathCalculation(8);
 
-console.log(singer.getAutograph());
+console.log(MathCalculation.createArray());
