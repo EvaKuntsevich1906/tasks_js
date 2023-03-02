f2fca87f2d2486870247904f11b32a7b55f5fd2d
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-03-02 22:52:24 +0300

    9 task

--- OOP/9/app.js
+++ OOP/9/app.js
@@ -1,17 +1,36 @@
-// Реализуйте класс Singer, который будет иметь следующие свойства: name,
-// surname. Также класс должен иметь метод getАutograph, который будет выводить
-// “{name} {surname), с наилучшими пожеланиями”. 
+// 8. Реализуйте класс Validator. У него будет метод isEmail параметром принимает
+// строку и проверяет, является ли она корректным емейлом или нет. Если является
+// - возвращает true, если не является - то false.
+// 9 Дополнить класс Validator. Добавить метод isURL для проверки на url.
 
-class Singer {
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
+class Validator {
+    constructor(email, url) {
+        this.email = email;
+        this.url = url;
     }
 
-    getAutograph() {
-        return `${this.name} ${this.surname}  с наилучшими пожеланиями`
+    isValid(email) {
+        try {
+            if (!/^[a-z]+@[a-z]+\.[a-z]{1,5}$/gm.test(email)) throw new Error("Некоректный ввод, false")
+            return true
+        } catch (err) {
+            alert(err.message)
+        }
+    }
+
+    isURL(url) {
+        this.isValid = true;
+        try {
+            if (!/^[http | https]+\:\/\/[a-zA-Zа-яА-Я0-9]+\.[а-яА-ЯA-Za-z]+\/$/gm.test(url))  throw new Error("Это не url")
+            return true
+        } catch (err) {
+            alert(err.message)
+        }
     }
 }
-const singer = new Singer(`John`, `Smith`);
 
-console.log(singer.getAutograph());
+const emailValue = prompt("Введите электронную почту");
+const urlValue = prompt("Введите URL")
+const validator = new Validator();
+console.log(validator.isValid(emailValue));
+console.log(validator.isURL(urlValue));
