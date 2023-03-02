41dd31e27942a12e2880d862eed84b3fde0b2f4b
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-03-02 22:45:27 +0300

    task

--- OOP/8/app.js
+++ OOP/8/app.js
@@ -1,50 +1,23 @@
+// 8. Реализуйте класс Validator. У него будет метод isEmail параметром принимает
+// строку и проверяет, является ли она корректным емейлом или нет. Если является
+// - возвращает true, если не является - то false.
 
-// Реализуйте класс ServerById. Обязательными функциями считаются функции
-// middleware, controller, service, repository. Цепочка взаимодействия между
-// методами следующая:
-// middleware -> controller -> service -> repository, где:
-// middleware – функция валидатор
-// controller – функция, принимающая данные. Принимает json
-// service – функция проверки на то что с repository вернулось значение
-// repository – функция, симулирующая БД. Хранит массив данных. Взаимодействие с
-// этим массивом осуществляется только в repository. Массив находится в
-// приложении
-// Задание:
-// на вход подается JSON вида:
-// `{
-// "id": "javascript"
-// }`
-// Необходимо вывести в консоль найденный элемент массива по id если таковой
-// имеется. В противном случае бросить исключение. Добавить проверки 
 
-class ServerGetAll {
-    controller(obj) {
+class Validator {
+    constructor(email) {
+        this.email = email;
+    }
+
+    isValid(email) {
         try {
-            return this.service(obj);
-        } catch (error) {
-            return error.message;
+            if (!/^[a-z]+@[a-z]+\.[a-z]{1,5}$/gm.test(email)) throw new Error("Некоректный ввод, false")
+            return true
+        } catch (err) {
+            alert(err.message)
         }
-    }
-
-    service(obj) {
-        return this.repository(obj);
-    }
-
-    repository(obj) {
-        const array = [
-            { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
-            { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
-            { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
-            { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
-            { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
-        ];
-        const filtered = array.filter((el) => el.id === obj.id);
-        if (filtered.length === 0) throw new Error('id не существует');
-        return filtered;
     }
 }
 
-const obj = { "id": "javascript" }
-
-const serverGetAll = new ServerGetAll();
-console.log(serverGetAll.controller(obj));
+const emailValue = prompt("Введите электронную почту")
+const validator = new Validator();
+console.log(validator.isValid(emailValue));
