5c9067d90138276b55bb22d58441b54a66977225
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-03-02 23:01:26 +0300

    10 task in folder OOP

--- OOP/10/app.js
+++ OOP/10/app.js
@@ -4,14 +4,15 @@
 // 9 Дополнить класс Validator. Добавить метод isURL для проверки на url.
 
 class Validator {
-    constructor(email, url) {
+    constructor(email, url,data) {
         this.email = email;
         this.url = url;
+        this.data = data;
     }
 
     isValid(email) {
         try {
-            if (!/^[a-z]+@[a-z]+\.[a-z]{1,5}$/gm.test(email)) throw new Error("Некоректный ввод, false")
+            if (!/^[a-z]+@[a-z]+\.[a-z]{1,5}$/gm.test(email)) throw new Error("Некоректный ввод, false");
             return true
         } catch (err) {
             alert(err.message)
@@ -21,16 +22,28 @@ class Validator {
     isURL(url) {
         this.isValid = true;
         try {
-            if (!/^[http | https]+\:\/\/[a-zA-Zа-яА-Я0-9]+\.[а-яА-ЯA-Za-z]+\/$/gm.test(url)) throw new Error("Это не url")
+            if (!/^[http | https]+\:\/\/[a-zA-Zа-яА-Я0-9]+\.[а-яА-ЯA-Za-z]+\/$/gm.test(url)) throw new Error("Это не url");
             return true
         } catch (err) {
             alert(err.message)
         }
     }
+
+    isData(data) {
+        try {
+            if (!/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/gm.test(data)) throw new Error("Некорректно введена дата");
+            return true
+        } catch (err) {
+            alrt(err.messag)
+        }
+    }
 }
 
 const emailValue = prompt("Введите электронную почту");
-const urlValue = prompt("Введите URL")
+const urlValue = prompt("Введите URL"); 
+const dataValue = prompt("Введите дату");
+
 const validator = new Validator();
 console.log(validator.isValid(emailValue));
 console.log(validator.isURL(urlValue));
+console.log(validator.isData(dataValue));
