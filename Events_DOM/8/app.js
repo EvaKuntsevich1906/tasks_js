9d5efdb875ee4df5d72ceeb844218479a01d8e6b
evakuntsevich19 <evakuntsevich1906@gmail.com>
2023-08-29 23:32:57 +0300

    new task in forlder JSX

--- Events_DOM/8/app.js
+++
@@ -1,7 +0,0 @@
-// У вас есть кнопка. После нажатия поменять background.
-
-const button = document.querySelector(".btn");
-
-button.addEventListener("click", function newStyle() {
-    (button.style.backgroundColor == "blue") ? button.style.backgroundColor = "red" : button.style.backgroundColor = "blue"
-})
--- Events_DOM/9/app.js
+++
@@ -1,7 +0,0 @@
-// У вас есть кнопка. После нажатия поменять background. ТАКАЯ ЖЕ КАК И 8
-
-const button = document.querySelector(".btn");
-
-button.addEventListener("click", function newStyle() {
-    (button.style.backgroundColor == "blue") ? button.style.backgroundColor = "red" : button.style.backgroundColor = "blue"
-})
